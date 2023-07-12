0046e10f572fd83f95403cdac94bf66c83903d5d
AKorolGB <Alexander94.13@mail.ru>
2023-07-12 17:23:17 +0200

    dz5b

--- src/Les31.java
+++ src/Les31.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 //Пусть дан произвольный список целых чисел, удалить из него четные числа
-public class Les41 {
+public class Les31 {
     public static void main(String[] args) {
         List<Integer> numbers = generateRandomNambers(10,1,100);
         System.out.println(numbers);
